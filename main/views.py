2b4fcbcd281ca66244b7cc967088c2d51e7a7ceb
MinaLMG <beandro6@gmail.com>
2021-08-05 14:54:35 +0200

    replaced HttpResponse with context in home function

--- main/views.py
+++ main/views.py
@@ -3,7 +3,11 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1> Hello world from home page </h1>')
+    #return HttpResponse('<h1> Hello world from home page </h1>')
+    return render(request,'main/home.html',{
+        'title':'home'
+    })
 
 def about(request):
-    return HttpResponse("<h1> Hello world from about page </h1>")    
+    #return HttpResponse("<h1> Hello world from about page </h1>")    
+     return render(request,'main/about.html')
