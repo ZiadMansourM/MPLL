866d976be4d02c6318fdaacc710f10b337faaaaa
MinaLMG <beandro6@gmail.com>
2021-08-09 20:12:22 +0200

    checking the access token

--- blog/models.py
+++ blog/models.py
@@ -13,3 +13,4 @@ class Post(models.Model):
     date_posted = models.DateTimeField(default=timezone.now)
     # TODO: Look me up
     last_modifed = models.DateTimeField(auto_now=True)
+    #checking comment
