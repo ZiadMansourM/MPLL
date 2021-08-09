934c30014ecb0db75217cf0f22849dfdbadf198e
MohamedWZS <mohamedwael.31.8.2000@gmail.com>
2021-08-09 21:15:31 +0200

    changed the dummy database with actual one

--- blog/views.py
+++ blog/views.py
@@ -1,38 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
 
-
-# {{ post.date_posted|date:"F d, Y - P" }}
-posts = [
-    {
-        "img": "path",
-        "title": "Thanawya AMMA",
-        "content": """Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
-        pariatur praesentium temporibus dolorum cum corporis?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
-        pariatur praesentium temporibus dolorum cum corporis? Optio, dignissimos error. 
-        Sit ex magni eaque nesciunt exercitationem aliquid minus amet explicabo rerum. At?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
-        pariatur praesentium temporibus dolorum cum corporis? Optio, dignissimos error. 
-        Sit ex magni eaque nesciunt exercitationem aliquid minus amet explicabo rerum. At?Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque
-        pariatur praesentium temporibus dolorum cum corporis? Optio, dignissimos error. 
-        Sit ex magni eaque nesciunt exercitationem aliquid minus amet explicabo rerum. At? Optio, dignissimos error. 
-        Sit ex magni eaque nesciunt exercitationem aliquid minus amet explicabo rerum. At?""",
-        "author": "Ziad Mansour Mohamed",
-        "date_posted": "August 10, 2021 - 12:30 a.m.",
-        "last_modified": "August 11, 2021 - 1:30 p.m.",
-    },
-    {
-        "img": "path",
-        "title": "Test Driven Development",
-        "content": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque pariatur praesentium temporibus dolorum cum corporis? Optio, dignissimos error. Sit ex magni eaque nesciunt exercitationem aliquid minus amet explicabo rerum. At?",
-        "author": "Mando Wael",
-        "date_posted": "August 15, 2021 - 12:30 a.m.",
-        "last_modified": "",
-    }
-]
-
-
-# Create your views here.
-def home(requests):
-    context = {
-        "posts": posts
-    }
-    return render(requests, 'blog/home.html', context)
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
