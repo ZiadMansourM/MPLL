e9c60e001e470dd628ce6dec97d9e5f675c79876
MinaLMG <beandro6@gmail.com>
2021-08-09 20:03:33 +0200

    renamed events/' pictures file

--- blog/models.py
+++ blog/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
     title = models.CharField(max_length=120)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    image = models.ImageField(default='default.jpg', upload_to='pics')
     date_posted = models.DateTimeField(default=timezone.now)
     # TODO: Look me up
     last_modifed = models.DateTimeField(auto_now=True)
