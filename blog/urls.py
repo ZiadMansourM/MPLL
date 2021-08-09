433c72fd5710afe549b6b64d399be014a122b3e5
MohamedWZS <mohamedwael.31.8.2000@gmail.com>
2021-08-09 23:30:37 +0200

    created blog CRUD templates

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path, include
-from .views import PostListView
+from .views import (
+    PostListView, 
+    PostDetailView,
+    PostCreateView,
+    PostDeleteView,
+    PostUpdateView
+)
+
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
+    path('<int:pk>/delete', PostDeleteView.as_view(), name='blog-delete'),
+    path('<int:pk>/update', PostUpdateView.as_view(), name='blog-update'),
+    path('<int:pk>/', PostDetailView.as_view(), name='blog-detail'),
+    path('new/', PostCreateView.as_view(), name='blog-create'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,47 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/detailed.html'
+    context_object_name = 'post'
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content', 'image']
+    template_name = 'blog/create.html' 
+    context_object_name = 'post'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url='/'
+    template_name = 'blog/post_confirm_delete.html'  
+    
+    def test_func(self):
+        post = self.get_object()
+        return (self.request.user == post.author)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content', 'image']
+    template_name = 'blog/update.html' 
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return (self.request.user == post.author)
