9256ee5c6e3a42687342cae264b9a67125c5214d
MohamedWZS <mohamedwael.31.8.2000@gmail.com>
2021-08-09 21:13:18 +0200

    updated the blog list view

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
-from . import views
+from .views import PostListView
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
 ]
