8bf5f8f813ad6d1e70b327e20d6fbe680a91ee62
MinaLMG <beandro6@gmail.com>
2021-08-10 22:58:21 +0200

    customized different user authentication related urls

---
+++ users/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from users import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('registermanager/', views.registermanager, name='register-manager'),
+    path('registereditor/', views.registereditor, name='register-editor'),
+    path('profile/', views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+]
