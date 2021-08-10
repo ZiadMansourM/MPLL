9ef48e5628a8d077896ec48c21e0601f49472674
MinaLMG <beandro6@gmail.com>
2021-08-10 20:28:48 +0200

    set PostCreateView to use PostCreateForm

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .forms import PostCreateForm
+
 
 class PostListView(ListView):
     model = Post
@@ -9,34 +11,39 @@ class PostListView(ListView):
     context_object_name = 'posts'
     ordering = ['-date_posted']
 
+
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/detailed.html'
     context_object_name = 'post'
 
+
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
-    fields = ['title', 'content', 'image']
-    template_name = 'blog/create.html' 
+    # fields = ['title', 'content', 'image']
+    template_name = 'blog/create.html'
     context_object_name = 'post'
+    form_class = PostCreateForm
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
+
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
-    success_url='/'
-    template_name = 'blog/post_confirm_delete.html'  
-    
+    success_url = '/'
+    template_name = 'blog/post_confirm_delete.html'
+
     def test_func(self):
         post = self.get_object()
         return (self.request.user == post.author)
 
+
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ['title', 'content', 'image']
-    template_name = 'blog/update.html' 
+    template_name = 'blog/update.html'
 
     def form_valid(self, form):
         form.instance.author = self.request.user
