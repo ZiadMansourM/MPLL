71cb4e8c34fedeb6dad31ce7de626bd0d4b95539
MinaLMG <beandro6@gmail.com>
2021-08-10 20:24:27 +0200

    customized admin page for post model

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from .models import Post
 # Register your models here.
 
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'author', 'date_posted')
+    list_filter = ('author__username',)
+    search_fields = ('author__username', 'title')
+    ordering = ('-date_posted',)
