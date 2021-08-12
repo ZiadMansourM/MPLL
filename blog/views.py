862a3125b9bd0612d2dc6a7a527759ad644370d3
MohamedWZS <mohamedwael.31.8.2000@gmail.com>
2021-08-12 19:04:03 +0200

    TO BE FIXED LATER post detailed view

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,57 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Post
+from .models import Comment, Post
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from .forms import PostCreateForm
+from .forms import PostCreateForm, CommentCreateForm
 
 
 class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html'
     context_object_name = 'posts'
-    ordering = ['-date_posted']
+    ordering = ['-is_pinned','-date_posted']
+    paginate_by = 5
 
 
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/detailed.html'
     context_object_name = 'post'
+
+    # TODO: to be fixed
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['form'] = CommentCreateForm
+
+    #     return context
+
+    # def get(self, request, *args, **kwargs):
+    #     form = CommentCreateForm()
+    #     self.object = self.get_object()
+    #     context = super().get_context_data(**kwargs)
+    #     context['form'] = CommentCreateForm
+
+    #     return self.render_to_response(context=context)
+
+    # def post(self, request, *args, **kwargs):
+    #     form = CommentCreateForm(request.POST)
+
+    #     if form.is_valid():
+    #         comment = form.save(commit=False)
+    #         comment.owner = request.user
+    #         comment.post = self.get_object()
+    #         comment.save()
+    #         self.object = self.get_object()
+    #         context = super().get_context_data(**kwargs)
+    #         context['form'] = CommentCreateForm
+
+    #     else:
+    #         self.object = self.get_object()
+    #         context = super().get_context_data(**kwargs)
+    #         context['form'] = form
+
+
+    #     return self.render_to_response(context=context)
 
 
 class PostCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
@@ -43,7 +79,7 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
-    fields = ['title', 'content', 'image']
+    fields = ['title', 'content', 'is_pinned', 'image']
     template_name = 'blog/update.html'
 
     def form_valid(self, form):
