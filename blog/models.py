5d887caed6e5f290b1b1383105462d902751de67
MohamedWZS <mohamedwael.31.8.2000@gmail.com>
2021-08-12 19:02:39 +0200

    added save function and created Comment and Reply models

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.conf import settings
+from django.db.models.fields.related import ForeignKey
 from django.utils import timezone
 from django.urls import reverse
 from ckeditor.fields import RichTextField
 from django.utils.translation import gettext_lazy as _
+from .parser import cleanhtml
 
 User = settings.AUTH_USER_MODEL
 
@@ -11,6 +13,7 @@ User = settings.AUTH_USER_MODEL
 class Post(models.Model):
     title = models.CharField(max_length=120)
     content = RichTextField()
+    snippet = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     image = models.ImageField(default='default.jpg', upload_to='pics')
     date_posted = models.DateTimeField(default=timezone.now)
@@ -32,10 +35,36 @@ class Post(models.Model):
         ordering = ['title']
         db_table = 'posts'
 
-    # def save(self, *args, **kwargs):
-    #     try:
-    #         this = Post.objects.get(id=self.id)
-    #         if this.image.name != self.image.name and this.image.name != "default.jpg":
-    #             this.MyImageFieldName.delete()
-    #     except: pass
-    #     super(Post, self).save(*args, **kwargs)
+    def save(self, *args, **kwargs):
+        # try:
+        #     this = Post.objects.get(id=self.id)
+        #     if this.image.name != self.image.name and this.image.name != "default.jpg":
+        #         this.MyImageFieldName.delete()
+        # except: pass
+        self.snippet = cleanhtml(self.content)
+        super(Post, self).save(*args, **kwargs)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    owner =  models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    comment = models.TextField()
+    date_posted = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"{self.post}-{self.owner}"
+
+    class Meta:
+        db_table = 'comments'
+
+class Reply(models.Model):
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name="replies")
+    owner =  models.ForeignKey(User, on_delete=models.CASCADE, related_name="replies")
+    reply = models.TextField()
+    date_posted = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"{self.comment}-{self.owner}"
+
+    class Meta:
+        db_table = 'replies'
+        verbose_name_plural = "Replies"
