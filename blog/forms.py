aac973542f37ce264b1cfbb43f18761d2c549a1b
MohamedWZS <mohamedwael.31.8.2000@gmail.com>
2021-08-12 19:01:26 +0200

    created CommentCreateForm model

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,14 @@
 from django import forms 
-from .models import Post
+from .models import Comment, Post
 from ckeditor.widgets import CKEditorWidget
 
 class PostCreateForm(forms.ModelForm):
     content = forms.CharField(widget=CKEditorWidget())
     class Meta:
         model = Post
-        fields =['title', 'content', 'image']
+        fields =['title', 'is_pinned', 'content', 'image']
+
+class CommentCreateForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields =['comment']
