5f38bd004d631f4f4c6f92ec922b4a9186f5bfc5
MinaLMG <beandro6@gmail.com>
2021-08-18 12:13:32 +0200

    fixed old Code class related bugs

--- booking/views.py
+++ booking/views.py
@@ -1,15 +1,8 @@
 from django.shortcuts import render
-from booking.models import Code, Book
+from booking.models import DeweyDecimalClassification, Book
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 # Create your views here.
 
-
-# def home(request):
-#     codes=Code.objects.all()
-#     return render(request, 'booking/home.html', {
-#         'title': 'booking',
-#         'codes': codes
-#     })
 
 
 class BookListView(ListView):
@@ -19,7 +12,7 @@ class BookListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['codes'] = Code.objects.all()
+        context['codes'] = DeweyDecimalClassification.objects.all()
         context['title'] = 'Booking'
         return context
 
