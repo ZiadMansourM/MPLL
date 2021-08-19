fe7377e77871ef21803c0341098d905456bf6690
MinaLMG <beandro6@gmail.com>
2021-08-19 12:43:13 +0200

    included some pathes

--- booking/urls.py
+++ booking/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
-from .views import BookListView ,BookDetailView
+from .views import BookCreateView, BookListView ,BookDetailView,BookDeleteView,BookUpdateView
 urlpatterns = [
     path('', BookListView.as_view(), name='booking-home'),
-    path('<int:pk>/', BookDetailView.as_view(), name='booking-detail'),
+    path('<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+    path('new/',BookCreateView.as_view(),name='book-create'),
+    path('<int:pk>/delete', BookDeleteView.as_view(), name='book-delete'),
+    path('<int:pk>/update', BookUpdateView.as_view(), name='book-update'),
 ]
