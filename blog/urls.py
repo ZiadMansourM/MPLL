914276cb75ab49d3087807f4a13d01fa157024da
ZiadMansourM <ziadmansour.4.9.2000@gmail.com>
2021-08-20 01:58:03 +0200

    updated urls paths

--- blog/urls.py
+++ blog/urls.py
@@ -14,10 +14,10 @@ from .views import (
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('contactus/', contact_us, name="contact-us"),
-    path('<int:pk>/delete', PostDeleteView.as_view(), name='blog-delete'),
-    path('<int:pk>/update', PostUpdateView.as_view(), name='blog-update'),
-    path('<int:pk>/comment/', CommentCreateView.as_view(), name='comment-create'),
-    path('<int:pk>/comment/<int:id>', ReplyCreateView.as_view(), name='reply-create'),
-    path('<int:pk>/', PostDetailView.as_view(), name='blog-detail'),
+    path('<uuid:pk>/delete', PostDeleteView.as_view(), name='blog-delete'),
+    path('<uuid:pk>/update', PostUpdateView.as_view(), name='blog-update'),
+    path('<uuid:pk>/comment/', CommentCreateView.as_view(), name='comment-create'),
+    path('<uuid:pk>/comment/<uuid:id>', ReplyCreateView.as_view(), name='reply-create'),
+    path('<uuid:pk>/', PostDetailView.as_view(), name='blog-detail'),
     path('new/', PostCreateView.as_view(), name='blog-create')
 ]
