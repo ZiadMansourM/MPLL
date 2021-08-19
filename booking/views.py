9b74a3e6cb3637ef4b4387ac75518338cec6a0dc
MinaLMG <beandro6@gmail.com>
2021-08-19 12:44:34 +0200

    added Book related view

--- booking/views.py
+++ booking/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from booking.models import DeweyDecimalClassification, Book
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .forms import BookCreateForm
+
 # Create your views here.
 
 
@@ -21,3 +24,31 @@ class BookDetailView(DetailView):
     template_name = 'booking/detailed.html'
     context_object_name = 'book'
 
+class BookCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+    model = Book
+    template_name = 'booking/create.html'
+    context_object_name = 'book'
+    form_class = BookCreateForm
+
+    def test_func(self):
+        return self.request.user.is_editor
+
+class BookDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Book
+    success_url = '/booking'
+    template_name = 'booking/book_confirm_delete.html'
+
+    def test_func(self):
+        return (self.request.user.is_editor)
+
+
+class BookUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Book
+    fields =  ['name', 'author', 'classification', 'code',
+                  'pages_num', 'date_published', 'date_added', 'publisher',
+                  'available_copies', 'available_borrowing', 'available',
+                  'image', 'brief']
+    template_name = 'booking/update.html'
+
+    def test_func(self):
+        return (self.request.user.is_editor)
