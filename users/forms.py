75a3a4d548c41420bedfed07333435c3964c94a3
ZiadMansourM <ziadmansour.4.9.2000@gmail.com>
2021-08-20 01:01:01 +0200

    fixed User model import and form fields

--- users/forms.py
+++ users/forms.py
@@ -1,29 +1,29 @@
 from django import forms
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django.conf import settings
-from .models import CustomUser
 
-# TODO : AttributeError: 'str' object has no attribute '_meta'
-# User = settings.AUTH_USER_MODEL
-
+CustomUser = get_user_model()
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField(required=False)
+    email = forms.EmailField(required=True)
 
     class Meta:
         model = CustomUser
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'email','first_name', 'last_name', 'password1', 'password2']
 
 class MLERegisterForm(UserCreationForm): 
     # MLE : manager librarian editor
     # set password "1,2" to None to hide them from the form
+    email = forms.EmailField(required=True)
     password1 = None 
     password2 = None 
     
     class Meta:
-        model = CustomUser # form.save
+        model = CustomUser
         fields = [
             'username',
+            'email',
             'first_name',
             'last_name'
         ]
