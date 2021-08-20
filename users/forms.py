6dc222480c1e23755586fe89a801327c22dd493e
ZiadMansourM <ziadmansour.4.9.2000@gmail.com>
2021-08-20 19:50:50 +0200

    added user UpdateProfileForm

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.conf import settings
 
 CustomUser = get_user_model()
@@ -10,7 +10,19 @@ class UserRegisterForm(UserCreationForm):
 
     class Meta:
         model = CustomUser
-        fields = ['username', 'email','first_name', 'last_name', 'password1', 'password2']
+        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
+
+class ProfileUpdateForm(UserChangeForm):
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'first_name', 'last_name', 'telephone']
+        widgets = {
+            'first_name': forms.TextInput(attrs={"class": "form-control bg-light"}),
+            'last_name': forms.TextInput(attrs={"class": "form-control bg-light"}),
+            'username': forms.TextInput(attrs={"class": "form-control bg-light"}),
+            'email': forms.EmailInput(attrs={"class": "form-control bg-light"}),
+            'telephone': forms.TextInput(attrs={"class": "form-control bg-light"}),
+        }
 
 class MLERegisterForm(UserCreationForm): 
     # MLE : manager librarian editor
