24b47e4e34dd19284cc967938102825dbe97605f
ZiadMansourM <ziadmansour.4.9.2000@gmail.com>
2021-08-21 03:00:25 +0200

    updated urls paths

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from .views import (
+    report,
     contact_us,
     PostListView, 
     PostDetailView,
@@ -7,17 +8,22 @@ from .views import (
     PostDeleteView,
     PostUpdateView,
     CommentCreateView,
-    ReplyCreateView
+    ReplyCreateView,
+    CommentDetailView,
+    ReplyDetailView
 )
 
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('contactus/', contact_us, name="contact-us"),
-    path('<uuid:pk>/delete', PostDeleteView.as_view(), name='blog-delete'),
+    path('report/', report, name="report"),
     path('<uuid:pk>/update', PostUpdateView.as_view(), name='blog-update'),
+    path('<uuid:pk>/delete', PostDeleteView.as_view(), name='blog-delete'),
     path('<uuid:pk>/comment/', CommentCreateView.as_view(), name='comment-create'),
-    path('<uuid:pk>/comment/<uuid:id>', ReplyCreateView.as_view(), name='reply-create'),
+    path('<uuid:pk>/comment/<uuid:id>/reply/<uuid:num>', ReplyDetailView.as_view(), name='Comment-detail'),
+    path('<uuid:pk>/comment/<uuid:id>/reply', ReplyCreateView.as_view(), name='reply-create'),
+    path('<uuid:pk>/comment/<uuid:id>', CommentDetailView.as_view(), name='Comment-detail'),
     path('<uuid:pk>/', PostDetailView.as_view(), name='blog-detail'),
     path('new/', PostCreateView.as_view(), name='blog-create')
 ]
