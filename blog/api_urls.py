3b1c1e7bb711ee7fd32849727570a1dab6769e45
ZiadMansourM <ziadmansour.4.9.2000@gmail.com>
2021-08-23 01:22:58 +0200

    added pathes to CommmentJsonCreateView and ReplyJsonCreateView

--- blog/api_urls.py
+++ blog/api_urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from .api_views import (
         CommmentJsonCreateView,
-        CommmentJsonListView
+        CommmentJsonListView,
+        ReplyJsonCreateView
     )
 
 
 urlpatterns = [
-    path('<uuid:pk>/comment/create', CommmentJsonCreateView.as_view(), name='api-comment-create'),
+    path('<uuid:pk>/comment/new', CommmentJsonCreateView.as_view(), name='api-comment-create'),
     path('<uuid:pk>/comment/', CommmentJsonListView.as_view(), name='api-comment-list'),
+    path('<uuid:pk>/comment/<uuid:id>/reply/new', ReplyJsonCreateView.as_view(), name='api-reply-create'),
 ]
