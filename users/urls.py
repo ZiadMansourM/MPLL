0ae3c724167c33082ba79c95303fbc13720f3510
ZiadMansourM <ziadmansour.4.9.2000@gmail.com>
2021-08-24 04:44:35 +0200

    added deactivate path

--- users/urls.py
+++ users/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('registermanager/', views.registermanager, name='register-manager'),
     path('registereditor/', views.registereditor, name='register-editor'),
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
+    path('deactivate/', views.deactivate, name="deactivate"),
     path('profile/', views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
