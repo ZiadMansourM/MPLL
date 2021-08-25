c744942c11e460ea1d28b63e3be393abb0001894
MinaLMG <beandro6@gmail.com>
2021-08-25 18:11:33 +0200

    removed a duplicated path

--- blog/urls.py
+++ blog/urls.py
@@ -27,7 +27,6 @@ urlpatterns = [
     path('<uuid:pk>/comment/<uuid:id>/delete', DeleteComment, name='comment-delete'),
     path('<uuid:pk>/comment/<uuid:id>/reply/<uuid:num>', ReplyDetailView.as_view(), name='Comment-detail'),
     path('<uuid:pID>/comment/<uuid:cID>/reply/<uuid:rID>/delete', DeleteReply, name='reply-delete'),
-    path('<uuid:pk>/comment/<uuid:id>/delete', DeleteComment, name='comment-delete'),
     path('<uuid:pk>/comment/<uuid:id>', CommentDetailView.as_view(), name='Comment-detail'),
     path('<uuid:pk>/', PostDetailView.as_view(), name='blog-detail'),
     path('new/', PostCreateView.as_view(), name='blog-create'),
