cdc1e4a9940cdef86c78aad1d195bb8d53518b5d
MinaLMG <beandro6@gmail.com>
2021-08-25 18:03:50 +0200

    added CityCreateJsonView

--- booking/api_urls.py
+++ booking/api_urls.py
@@ -2,12 +2,14 @@ from django.urls import path, include
 from .api_views import (
         AuthorCreateJsonView, 
         AuthorListJsonView, 
-        PublisherCreateJsonView
+        PublisherCreateJsonView,
+        CityCreateJsonView
     )
 
 
 urlpatterns = [
     path('author/create/', AuthorCreateJsonView.as_view(), name='api-author-create'),
     path('publisher/create/', PublisherCreateJsonView.as_view(), name='api-publisher-create'),
+    path('city/create/', CityCreateJsonView.as_view(), name='api-city-create'),
     path('author/', AuthorListJsonView.as_view(), name='api-author-list'),
 ]
