61feee6debe78443b32a0f4fa10e1d097f7af4b2
MinaLMG <beandro6@gmail.com>
2021-08-25 18:04:25 +0200

    added CityCreateJsonView

--- booking/api_views.py
+++ booking/api_views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import View
 from django.http import HttpResponse, JsonResponse
 
-from .models import Author, Publisher
+from .models import Author, Publisher,City
 from .forms import AuthorCreateForm, PublisherCreateForm
 
 
@@ -29,6 +29,18 @@ class PublisherCreateJsonView(LoginRequiredMixin, UserPassesTestMixin, View):
         publisher = Publisher.objects.create(name=name)
         publisher = list(Publisher.objects.filter(name=name).values())
         return JsonResponse(publisher, safe=False)
+
+    def test_func(self):
+        return (self.request.user.is_editor)
+
+
+class CityCreateJsonView(LoginRequiredMixin, UserPassesTestMixin, View):
+
+    def post(self, *args, **kwargs):
+        name = self.request.POST['name']
+        city = City.objects.create(name=name)
+        city = list(City.objects.filter(name=name).values())
+        return JsonResponse(city, safe=False)
 
     def test_func(self):
         return (self.request.user.is_editor)
