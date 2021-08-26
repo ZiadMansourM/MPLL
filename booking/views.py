349f54419f9ce861711ab86cc0a50e166306820c
ZiadMansourM <ziadmansour.4.9.2000@gmail.com>
2021-08-26 03:45:29 +0200

    formating suggested by mina

--- booking/views.py
+++ booking/views.py
@@ -11,7 +11,7 @@ class BookListView(ListView):
     model = Book
     template_name = 'booking/home.html'
     context_object_name = 'books'
-    paginate_by = 3
+    paginate_by = 10
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
