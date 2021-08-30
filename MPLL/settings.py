4814440078060045832b1e80ef8eb5bf9baa8f5f
ZiadMansourM <ziadmansour.4.9.2000@gmail.com>
2021-08-30 02:29:22 +0200

    added static root and fixed broken path

--- MPLL/settings.py
+++ MPLL/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'
 
 # Default primary key field type
