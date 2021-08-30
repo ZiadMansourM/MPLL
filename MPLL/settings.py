47923c7509ee8419480f97371778db2bb5d3feaa
ZiadMansourM <ziadmansour.4.9.2000@gmail.com>
2021-08-30 02:25:26 +0200

    set allowed host according to env

--- MPLL/settings.py
+++ MPLL/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config["DEBUG"]
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config["ALLOWED_HOSTS"]
 
 AUTH_USER_MODEL ='users.CustomUser'
 
