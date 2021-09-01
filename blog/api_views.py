56c60d9b0241a3b1e984c2a035be7838a092c2f4
MinaLMG <beandro6@gmail.com>
2021-09-01 02:01:29 +0200

    creaed PostJsonLikeView & PostJsonUnlikeView

--- blog/api_views.py
+++ blog/api_views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.shortcuts import get_object_or_404
 from django.views.generic import View
 from django.http import JsonResponse
 from django.contrib.auth import get_user_model
@@ -6,6 +7,7 @@ from django.contrib.auth import get_user_model
 from .models import Post, Comment, Reply
 
 User = get_user_model()
+
 
 class CommmentJsonCreateView(LoginRequiredMixin, View):
     def post(self, *args, **kwargs):
@@ -28,7 +30,8 @@ class CommmentJsonCreateView(LoginRequiredMixin, View):
 
 class CommmentJsonListView(View):
     def get(self, *args, **kwargs):
-        comments = list(Comment.objects.filter(post=self.kwargs['pk']).values())
+        comments = list(Comment.objects.filter(
+            post=self.kwargs['pk']).values())
         return JsonResponse(comments, safe=False)
 
 
@@ -47,9 +50,32 @@ class ReplyJsonCreateView(LoginRequiredMixin, View):
             'owner_username': reply.owner.username,
             'reply_id': reply.id,
             'reply': reply.reply,
-            'post_id':reply.comment.post.id,
-            'comment_id':reply.comment.id,
+            'post_id': reply.comment.post.id,
+            'comment_id': reply.comment.id,
             # 'reply_owner':reply.owner,
             # 'current_user':User,
+        }
+        return JsonResponse(response_dict, safe=False)
+
+
+class PostJsonLikeView(LoginRequiredMixin, View):
+    def post(self, *args, **kwargs):
+        post = get_object_or_404(Post, id=self.kwargs['pk'])
+        if not post.likes.filter(id=self.request.user.id).exists():
+            post.likes.add(self.request.user)
+        response_dict = {
+            'total_likes': post.total_likes()
+        }
+        return JsonResponse(response_dict, safe=False)
+
+
+class PostJsonUnlikeView(LoginRequiredMixin, View):
+
+    def post(self, *args, **kwargs):
+        post = get_object_or_404(Post, id=self.kwargs['pk'])
+        if post.likes.filter(id=self.request.user.id).exists():
+            post.likes.remove(self.request.user)
+        response_dict = {
+            'total_likes': post.total_likes()
         }
         return JsonResponse(response_dict, safe=False)
