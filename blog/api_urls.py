732b74071fb9ce7e1e87a69f59ada90d36af98e3
MinaLMG <beandro6@gmail.com>
2021-09-01 02:00:50 +0200

    creaed PostJsonLikeView & PostJsonUnlikeView related pathes

--- blog/api_urls.py
+++ blog/api_urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 from .api_views import (
         CommmentJsonCreateView,
         CommmentJsonListView,
+        PostJsonLikeView,
+        PostJsonUnlikeView,
         ReplyJsonCreateView
     )
 
@@ -10,4 +12,6 @@ urlpatterns = [
     path('<uuid:pk>/comment/new', CommmentJsonCreateView.as_view(), name='api-comment-create'),
     path('<uuid:pk>/comment/', CommmentJsonListView.as_view(), name='api-comment-list'),
     path('<uuid:pk>/comment/<uuid:id>/reply/new', ReplyJsonCreateView.as_view(), name='api-reply-create'),
+    path('<uuid:pk>/like/', PostJsonLikeView.as_view(), name='api-like-post'),
+    path('<uuid:pk>/unlike/', PostJsonUnlikeView.as_view(), name='api-unlike-post'),
 ]
